a7f6a303be13a698134bcde4c3443a0cea60fa31
Rafael Seemann <rseemann@gmail.com>
2016-07-06 12:39:13 +0200

    Add some tree textures

---
+++ Assets/src/scripts/TreeController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeController : MonoBehaviour {
+	private const string prefabName = "Assets/src/objects/Trunk"; 
+	private Object trunkPrefab = null;
+
+	void Start () {
+		Debug.Log (prefabName);
+		trunkPrefab = Resources.Load (prefabName);
+		Instantiate (trunkPrefab);
+	}
+	
+	void Update () {
+//		Instantiate (trunkPrefab);
+	}
+}
